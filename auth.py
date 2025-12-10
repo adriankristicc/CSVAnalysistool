62e5640eb6ca75b4f4385e6ec279ae5e55ec9cb7
adriankristic <adriankristic00@icloud.com>
2025-12-10 21:04:54 +0100

    test

--- auth.py
+++ auth.py
@@ -1,4 +1,4 @@
-# auth.py
+
 import hashlib
 from models import User
 from database import get_db
@@ -9,12 +9,12 @@ def hash_password(password: str):
     return hashlib.sha256(password.encode()).hexdigest()
 
 def register(username: str, password: str, db: Session):
-    # Provjera da li postoji
+
     user = db.query(User).filter(User.username == username).first()
     if user:
         raise HTTPException(status_code=400, detail="Username already exists")
 
-    # Kreiraj korisnika
+
     new_user = User(
         username=username,
         hashed_password=hash_password(password)
